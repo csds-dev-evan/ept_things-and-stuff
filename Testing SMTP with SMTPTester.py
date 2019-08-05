37fe3d7218089ea70015fb6b4e1a97bb5444c877
csds-dev-evan <eroyer17@clearskydigital.net>
2019-08-05 11:27:58 -0500

    Create Testing SMTP with SMTPTester.py

---
+++ Testing SMTP with SMTPTester.py
@@ -0,0 +1,13 @@
+Install::
+git clone https://github.com/xFreed0m/SMTPTester.git
+cd SMTPTester
+pip install -r requirments.txt
+
+./SMTPTester.py
+
+readme::
+usage: SMTPTester.py [-h] --targets TARGETS [-p PORT] --tester TESTER
+                     [-t TOADDR] [-f FROMADDR] [-d DATA] [-a ADDRESS]
+                     [-s SUBJECT] [-i] [-e] [-v] [--debug]
+
+
